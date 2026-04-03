6eabe2368e3a2aeec27ef47e70573d64f6f7c100
Sham Rathod <88047563+ShamRathod21@users.noreply.github.com>
2026-04-03 09:05:38 +0530

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,46 @@
+class TrieNode{
+    public:
+    unordered_map<char, TrieNode*> mp;
+    bool last = false;
+};
+class PrefixTree {
+public:
+    TrieNode* head;
+    PrefixTree() {
+        head = new TrieNode();
+        head->last = true;
+    }
+    
+    void insert(string word) {
+        TrieNode* curr = head;
+        for(auto i : word) {
+            if(curr->mp.find(i) == curr->mp.end()) {
+                curr->mp[i] = new TrieNode();
+                curr = curr->mp[i];
+            } else {
+                curr = curr->mp[i];
+            }
+        }
+        curr->last = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr = head;
+        for(auto i : word) {
+            if(curr->mp.find(i) == curr->mp.end())
+                return false;
+            curr = curr->mp[i];
+        }
+        return curr->last;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curr = head;
+        for(auto i : prefix) {
+            if(curr->mp.find(i) == curr->mp.end())
+                return false;
+            curr = curr->mp[i];
+        }
+        return true;
+    }
+};
